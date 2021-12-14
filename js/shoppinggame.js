f129013752e851775ad612eaf61e061ece913e8f
Clayton J. Wilkerson <clay2000lbs@gmail.com>
2021-12-14 15:10:48 -0600

    Update shoppinggame.js
    7-10 done

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -36,9 +36,9 @@ const dateDiff = (date1, date2) => {
 };
 
 // Here, use Object.defineProperty to create property - daysToExpire
-Object.defineProperty(Product.prototype, "daysToExpire", {
+Object.defineProperty(Product.prototype, 'daysToExpire', {
     get: function() { 
-        return dateDiff(Product.expiryDate, new Date()) 
+        return dateDiff(this.expiryDate, new Date()) ;
     }
 })
 
@@ -48,11 +48,32 @@ Product.prototype.getDetails = function() {
 }
 
 // Define the MagicProduct class here
-
+function MagicProduct(id, name, price, expiryDate, points, isBonus) {
+    Product.call(this, id, name, price, expiryDate);
+    this.points = points;
+    this.isBonus = isBonus;
+}
 
 // Establish inheritance between Product() & MagicProduct() here
+MagicProduct.prototype = Object.create(Product.prototype)
 
 // Define Rating class here
+class Rating {
+    constructor(rate) {
+        this.rate = '';
+    }
+    set rating(value) {
+        if (value > 1 && value <= 4) {
+            this.rate = "OK"
+        } else if (value >= 5 && value <= 7) {
+            this.rate = "GOOD"
+        } else if (value > 7) {
+            this.rate = 'EXCEPTIONAL'
+        } else {
+            this.rate = 'BAD'
+        }
+    }
+}
 
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
